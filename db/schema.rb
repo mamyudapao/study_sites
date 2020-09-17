f314f52cfbcf0a210c2f4219832ed201ed390464
Noshian Kenichi <nosikenit@gmail.com>
2020-09-17 10:05:48 +0900

    create posts controller and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_16_232729) do
+ActiveRecord::Schema.define(version: 2020_09_17_005527) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.integer "rating", limit: 1
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "avatar"
   end
 
 end
