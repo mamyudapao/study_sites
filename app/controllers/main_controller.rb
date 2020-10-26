0f9445348477a0be32f9878d6e77a12dd792b3a0
Noshian Kenichi <nosikenit@gmail.com>
2020-10-26 14:36:27 +0900

    mede temporty post form

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,5 +2,8 @@ class MainController < ApplicationController
 
   def index
     @posts = Post.all
+    @post = Post.new
   end
+
+
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,10 @@
 class PostsController < ApplicationController
 
-  
+  def index
+    @posts = Post.all
+    @post = Post.new
+  end
+  def create
+
+  end
 end
